91d96867a122986c980a35dfa2bdd68de9e78351
Pedro Garcia Rodriguez <pedgarrod@gmail.com>
2023-10-05 13:39:41 +0200

    Add Finder and Activity Monitor configuration settings to macos.sh script
    Signed-off-by: Pedro Garcia Rodriguez <pedgarrod@gmail.com>

--- macos.sh
+++ macos.sh
@@ -3,45 +3,73 @@
 set -o errexit
 set -o pipefail
 
-# General - Set computer name (as done via System Preferences → Sharing).
-sudo scutil --set ComputerName "Converge"
-sudo scutil --set HostName "Converge"
-sudo scutil --set LocalHostName "Converge"
-sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "Converge"
+# Activity Monitor - Show the main window when launching.
+defaults write com.apple.ActivityMonitor OpenMainWindow -bool true
+
+# Activity Monitor- Visualize CPU usage in the Activity Monitor Dock icon.
+defaults write com.apple.ActivityMonitor IconType -int 5
+
+# Activity Monitor - Show all processes.
+defaults write com.apple.ActivityMonitor ShowCategory -int 0
+
+# Activity Monitor - Sort Activity Monitor results by CPU usage.
+defaults write com.apple.ActivityMonitor SortColumn -string "CPUUsage"
+defaults write com.apple.ActivityMonitor SortDirection -int 0
 
 # General - Automatically quit printer app once the print jobs complete.
 defaults write com.apple.print.PrintingPrefs "Quit When Finished" -bool true
 
-# General - Disable the “Are you sure you want to open this application?” dialog.
+# General - Avoid creating .DS_Store files on network volumes.
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+# Finder - Disable the “Are you sure you want to open this application?” dialog.
 defaults write com.apple.LaunchServices LSQuarantine -bool false
 
 # General - Disable auto-correct.
 defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
 
+# General - Disable disk image verification.
+defaults write com.apple.frameworks.diskimages skip-verify -bool true
+
+# General - Disable disk image locked verification.
+defaults write com.apple.frameworks.diskimages skip-verify-locked -bool true
+
+# General - Disable disk image remote verification.
+defaults write com.apple.frameworks.diskimages skip-verify-remote -bool true
+
 # General - Disable the sound effects on boot.
 sudo nvram SystemAudioVolume=" "
 
-# General - Disable the warning before emptying the Trash.
-defaults write com.apple.finder WarnOnEmptyTrash -bool false
-
-# General - Expand save panel by default.
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
-defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
-
-# General - Expand print panel by default.
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
-defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
-
-# General - Save to disk (not to iCloud) by default.
-defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+# General - Set computer name.
+sudo scutil --set ComputerName "Converge"
+sudo scutil --set HostName "Converge"
+sudo scutil --set LocalHostName "Converge"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "Converge"
 
 # General - Set the timezone; see `systemsetup -listtimezones` for other values.
 systemsetup -settimezone "Europe/Madrid" > /dev/null
 
-# General - Show the ~/Library folder
+# Finder - Disable the warning before emptying the Trash.
+defaults write com.apple.finder WarnOnEmptyTrash -bool false
+
+# Finder - Disable the warning when changing a file extension.
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+# Finder - Expand save panel by default.
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+# Finder - Expand print panel by default.
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint -bool true
+defaults write NSGlobalDomain PMPrintingExpandedStateForPrint2 -bool true
+
+# Finder - Save to disk (not to iCloud) by default.
+defaults write NSGlobalDomain NSDocumentSaveNewDocumentsToCloud -bool false
+
+# Finder - Show the ~/Library folder
 chflags nohidden ~/Library
 
-# General - Show the ~/Users folder
+# Finder - Show the ~/Users folder
 chflags nohidden /Users
 
 # Transmission - Don’t prompt for confirmation before downloading.
